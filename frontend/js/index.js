a2fbcaaa7ea4c3fe26123cf6b5deabbb6ad906d7
ashrafsakib <ashrafulsakib740@gmail.com>
2025-04-01 01:32:01 -0700

    Registration

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -26,3 +26,42 @@ fetch('/nav/footer.html')
         console.error('Error loading footer:', error);
     });
 
+// taking all input values
+const nameInput = document.getElementById("username")
+const emailInput = document.getElementById("email")
+const passInput = document.getElementById("password")
+const confirmPassInput = document.getElementById("confirmPassword")
+
+
+// creating user
+document.getElementById("registrationForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const password = passInput.value;
+    const confirmPassword = confirmPassInput.value;
+
+
+    if(password == confirmPassword){
+        const response = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, password }),
+        });
+        const data = await response.json()
+        if(data?.status == 201){
+            nameInput.value=""
+            emailInput.value=""
+            passInput.value=""
+            confirmPassInput.value=""
+            alert(`${name}, your account created successfully`)
+        } else if(data?.status == 400){
+            alert("this email already is in use")
+        }
+    }else{
+        alert("password didn't match")
+    }
+});
+
